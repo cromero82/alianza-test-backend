5bd0118557d8e49f32a12ed66577f6bb551586a5
carlos romero <soportecarlosromero@gmail.com>
2020-12-07 13:06:45 -0500

    creacion inicial

---
+++ src/test/java/com/alianza/test/AlianzaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alianza.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlianzaClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
